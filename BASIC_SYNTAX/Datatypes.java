ecda15c8eee517a75d06287d32dd766b8268977f
JhanviMangtani <jhanvimangtani@gmail.com>
2025-09-12 13:23:53 +0530

    Add Datatypes.java covering primitive data types in java

---
+++ BASIC_SYNTAX/Datatypes.java
@@ -0,0 +1,15 @@
+public class Datatypes
+{
+    public static void main(String[] args)
+    {
+        int n =10;
+        double price = 8.5;
+        char grade = 'A';
+
+        System.out.println("Number : "+ n);
+        System.out.println("Price : "+ price);
+        System.out.println("Grade : "+ grade);
+
+    }
+    
+}
