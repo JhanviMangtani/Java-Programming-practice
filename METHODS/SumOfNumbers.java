4f3a8973cdffa43500f1933e20a7cd8fdbde4e1c
JhanviMangtani <jhanvimangtani@gmail.com>
2025-09-16 00:29:33 +0530

    Add method to find sum of two numbers

---
+++ METHODS/SumOfNumbers.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class SumOfNumbers 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter first number : ");
+        int a=sc.nextInt();
+
+        System.out.println("Enter second number : ");
+        int b=sc.nextInt();
+
+        int c= sum(a,b);
+        System.out.println("Sum of the two numbers is : "+c);
+
+    }
+
+    public static int sum(int x,int y)
+    {
+        return x+y;
+    }
+}
